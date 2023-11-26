7a982e3ba7e05250e735f365c5de288994622ff7
Kris Kuiper <kriskuiper74@gmail.com>
2023-11-26 12:32:11 +0100

    feat: add product detail page

---
+++ pages/products/[slug].vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="product-page">
+    <figure class="product-page__gallery">
+      <img
+        :src="product.image"
+        :alt="product.title"
+        fetchpriority="high"
+        class="product-page__gallery-image"
+      />
+    </figure>
+
+    <section class="product-page__content">
+      <h1>{{ product.title }}</h1>
+      <p>{{ product.description }}</p>
+
+      <button @click="addToCart">Add to cart</button>
+    </section>
+  </div>
+</template>
+
+<script setup lang="ts">
+const route = useRoute();
+
+// TODO: clean up data fetching from API
+const response = await fetch(
+  `https://fakestoreapi.com/products/${route.params.slug}`
+);
+const product = await response.json();
+
+function addToCart() {
+  console.warn("TODO: add to cart");
+}
+</script>
+
+<style lang="scss">
+.product-page {
+  display: grid;
+  gap: 32px;
+
+  @media (min-width: 768px) {
+    grid-template-columns: repeat(4, 1fr);
+  }
+}
+
+.product-page__gallery {
+  grid-column-start: 1;
+  grid-column-end: 3;
+}
+
+.product-page__gallery-image {
+  width: 100%;
+}
+
+.product-page__content {
+  grid-column-start: 3;
+  grid-column-end: -1;
+}
+</style>
