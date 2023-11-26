239321508673bd98e50c61e8a6a175efc1ca73f8
Kris Kuiper <kriskuiper74@gmail.com>
2023-11-26 12:31:47 +0100

    feat: add homepage with product cards

---
+++ components/product-card.vue
@@ -0,0 +1,31 @@
+<template>
+  <NuxtLink :to="href" :title="product.title" class="product-card">
+    <figure class="product-card__image-container">
+      <img
+        :src="product.image"
+        :alt="product.title"
+        class="product-card__image"
+      />
+    </figure>
+    <NuxtLink :to="`/products/${product.id}`"> to product </NuxtLink>
+  </NuxtLink>
+</template>
+
+<script setup lang="ts">
+import type { Product } from "@/types/api";
+
+type ProductCardProps = {
+  href: string;
+  product: Product;
+};
+
+defineProps<ProductCardProps>();
+</script>
+
+<style lang="scss">
+.product-card__image {
+  width: 100%;
+  aspect-ratio: 5/8;
+  object-fit: cover;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>Hello world!</h1>
+
+    <section class="product-grid">
+      <product-card
+        v-for="product in products"
+        :product="product"
+        :href="`/products/${product.id}`"
+      />
+    </section>
+  </div>
+</template>
+
+<script setup>
+// TODO: clean up data fetching from API
+const response = await fetch(
+  "https://fakestoreapi.com/products/category/women's clothing"
+);
+const products = await response.json();
+</script>
+
+<style lang="scss">
+.product-grid {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 1rem;
+}
+</style>
