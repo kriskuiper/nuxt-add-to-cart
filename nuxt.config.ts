ce4843d87613815e37645039e104107d17ea7cd7
Kris Kuiper <kriskuiper74@gmail.com>
2023-11-26 17:53:17 +0100

    feat: add global styling

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  css: ["@/assets/scss/app.scss"],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@import "@/assets/scss/_variables.scss";',
+        },
+      },
+    },
+  },
+  devtools: { enabled: true },
+});
