d6725e4034c0aae67afcd3a3a6ea52c99931905d
Kris Kuiper <kriskuiper74@gmail.com>
2023-11-26 19:14:28 +0100

    feat(toast): create component

---
+++ components/page-toast.vue
@@ -0,0 +1,27 @@
+<template>
+  <output class="page-toast">
+    <span class="page-toast__message">
+      {{ message }}
+    </span>
+
+    <button v-if="isClosable">
+      <span class="visually-hidden">Close</span>
+    </button>
+  </output>
+</template>
+
+<script setup lang="ts">
+type ToastProps = {
+  message: string;
+  isClosable?: boolean;
+};
+
+defineProps<ToastProps>();
+</script>
+
+<style>
+.page-toast {
+  border: 1px solid #ccc;
+  padding: 1rem;
+}
+</style>
