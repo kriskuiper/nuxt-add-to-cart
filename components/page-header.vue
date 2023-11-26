3b721b2adf4e26cffdbc213ea5b7b5f6105c8774
Kris Kuiper <kriskuiper74@gmail.com>
2023-11-26 19:13:53 +0100

    feat: add page header

---
+++ components/page-header.vue
@@ -0,0 +1,23 @@
+<template>
+  <header class="header">
+    <p class="header__logo">MyStore</p>
+  </header>
+</template>
+
+<style lang="scss">
+.header {
+  display: flex;
+  justify-content: center;
+  border-bottom: 1px solid lightgray;
+  padding: 1rem;
+
+  @media ($media-md) {
+    padding: 1.25rem 1rem;
+  }
+}
+
+.header__logo {
+  font-weight: bold;
+  font-size: 1.25rem;
+}
+</style>
