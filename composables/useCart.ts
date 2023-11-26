71c16dec76804d7e1c1740b2a95ebcec1f229209
Kris Kuiper <kriskuiper74@gmail.com>
2023-11-26 17:28:44 +0100

    refactor(useCart): reuse logic for updating cart

--- composables/useCart.ts
+++ composables/useCart.ts
@@ -1,30 +1,36 @@
 import type { Product } from "@/types/api";
 import { keys } from "@/lib/constants";
 
+type UpdateCartHandler = (items: Product[]) => void;
+type CartItemHandler = (item: Product) => void;
+
 // TODO: ideally move localStorage things to a seperate composable
 const useCart = () => {
   const cart = ref<Product[]>([]);
 
-  const addItem = (item: Product) => {
-    const updatedItems = [...cart.value, item];
-
-    cart.value = updatedItems;
-    window.localStorage.setItem(keys.CART, JSON.stringify(updatedItems));
+  const updateCart: UpdateCartHandler = (items) => {
+    cart.value = items;
+    window.localStorage.setItem(keys.CART, JSON.stringify(items));
   };
 
-  const removeItem = (item: Product) => {
+  const addItem: CartItemHandler = (item) => {
+    const updatedItems = [...cart.value, item];
+    updateCart(updatedItems);
+  };
+
+  const removeItem: CartItemHandler = (item) => {
     const filteredItems = cart.value.filter(
       (cartItem) => cartItem.id !== item.id
     );
-
-    cart.value = filteredItems;
-    window.localStorage.setItem(keys.CART, JSON.stringify(filteredItems));
+    updateCart(filteredItems);
   };
 
   onMounted(() => {
     const cartFromStorage = window.localStorage.getItem(keys.CART);
 
-    if (!cartFromStorage) return;
+    if (!cartFromStorage) {
+      return;
+    }
 
     cart.value = JSON.parse(cartFromStorage);
   });
